8db7db67f75393ac276fc8340d8fba094d1051de
blark <blark@McGrupp.local>
2016-03-13 14:55:05 -0400

    updated notice

--- python/RealSID.py
+++ python/RealSID.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python
+'''
+RealSID.py
+Version 1.1 - customized for espSID
+
+Copyright (c) 2013, A.T.Brask (atbrask[at]gmail[dot]com)
+All rights reserved,
+
+Very rudimentary SID player for demonstrating the RealSIDShield Arduino shield.
+Basically, it emulates a 6502 CPU with 64KB of memory. No VIC or CIA chips.
+It works by running the play routine at ~50 Hz. After each run, it updates the
+registers in real SID chip connected via the Arduino.
+
+This version has been modified to use TCP and output debug information about
+the SID register state.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
 
 import argparse
 import Queue
